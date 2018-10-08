275d61f4318c7e0d2a0d4befad2b5819bc3226ac
noushad-clickapps <noushad@clickapps.co>
2018-10-08 11:06:03 +0530

    Initial Commit

---
+++ TechnologyAssessmentTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechnologyAssessmentTest
+//
+//  Created by clickapps on 10/8/18.
+//  Copyright © 2018 Noushad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
